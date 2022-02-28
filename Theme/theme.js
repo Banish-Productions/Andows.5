6515d3e8f3eb4a61aa83a3e317ef3d1346f8dd25
educationeating <79743110+educationeating@users.noreply.github.com>
2022-02-28 21:21:30 +0530

    Add files via upload

---
+++ Theme/theme.js
@@ -0,0 +1,236 @@
+function e1() {
+  var audio = new Audio("Themechanged.wav");
+
+audio.play();
+  alert('bg applied');
+  document.getElementsByClassName("sidenav")[0].style.backgroundImage = "linear-gradient(to right,#00000055 , rgba(255,0,0,0.196))";
+  document.getElementById("mySidenav").style.width = "80%";
+  document.getElementById("close").style.position = "fixed";
+   document.getElementById("goto").style.position = "fixed";
+   document.getElementById("goto").style.right = "0%"
+   document.getElementById("goto").style.top = "-10px"
+   document.getElementById("goto").style.height = "100vh"
+   document.getElementById("goto").style.width = "20%"
+   document.getElementById("cover").style.position = "fixed" ;
+   document.getElementById("cover").style.left = "22%" ;
+   document.getElementById("settings").style.height = "0%";
+  document.getElementById("body").style.backgroundImage = "url(./Theme/1.jpg)";
+}
+
+function e2() {
+  var audio = new Audio("Themechanged.wav");
+
+audio.play();
+  alert('bg applied');
+  document.getElementsByClassName("sidenav")[0].style.backgroundImage = "linear-gradient(to right, #000db434 , rgba(193,3,3,0.094) , #00d1ff62)";
+  document.getElementById("mySidenav").style.width = "80%";
+  document.getElementById("close").style.position = "fixed";
+   document.getElementById("goto").style.position = "fixed";
+   document.getElementById("goto").style.right = "0%"
+   document.getElementById("goto").style.top = "-10px"
+   document.getElementById("goto").style.height = "100vh"
+   document.getElementById("goto").style.width = "20%"
+   document.getElementById("cover").style.position = "fixed" ;
+   document.getElementById("cover").style.left = "22%" ;
+   document.getElementById("settings").style.height = "0%";
+  document.getElementById("body").style.backgroundImage = "url(2.jpg)";
+  document.getElementById("body").style.backgroundImage = "url(./Theme/2.jpg)";
+}
+
+function e3() {
+  var audio = new Audio("Themechanged.wav");
+
+audio.play();
+  alert('bg applied');
+  document.getElementsByClassName("sidenav")[0].style.backgroundImage = "linear-gradient(to right, #00000034 , #79fff670 , #0affd862";
+  document.getElementById("mySidenav").style.width = "80%";
+  document.getElementById("close").style.position = "fixed";
+   document.getElementById("goto").style.position = "fixed";
+   document.getElementById("goto").style.right = "0%"
+   document.getElementById("goto").style.top = "-10px"
+   document.getElementById("goto").style.height = "100vh"
+   document.getElementById("goto").style.width = "20%"
+   document.getElementById("cover").style.position = "fixed" ;
+   document.getElementById("cover").style.left = "22%" ;
+   document.getElementById("settings").style.height = "0%";
+  document.getElementById("body").style.backgroundImage = "url(2.jpg)";
+  document.getElementById("body").style.backgroundImage = "url(./Theme/3.jpg)";
+}
+function e4() {
+  var audio = new Audio("Themechanged.wav");
+
+audio.play();
+  alert('bg applied');
+  document.getElementsByClassName("sidenav")[0].style.backgroundImage = "linear-gradient(to right, rgba(0,0,0,0.204) , rgba(255,121,121,0.309) , rgba(255,255,255,0.253)";
+  document.getElementById("mySidenav").style.width = "80%";
+  document.getElementById("close").style.position = "fixed";
+   document.getElementById("goto").style.position = "fixed";
+   document.getElementById("goto").style.right = "0%"
+   document.getElementById("goto").style.top = "-10px"
+   document.getElementById("goto").style.height = "100vh"
+   document.getElementById("goto").style.width = "20%"
+   document.getElementById("cover").style.position = "fixed" ;
+   document.getElementById("cover").style.left = "22%" ;
+   document.getElementById("settings").style.height = "0%";
+  document.getElementById("body").style.backgroundImage = "url(4.jpg)";
+  document.getElementById("body").style.backgroundImage = "url(./Theme/4.jpg)";
+}
+function e5() {
+  var audio = new Audio("Themechanged.wav");
+
+audio.play();
+  alert('bg applied');
+  document.getElementsByClassName("sidenav")[0].style.backgroundImage = "linear-gradient(to right, rgba(56,54,54,0.204) , rgba(131,1,1,0.167) , rgba(108,210,139,0.081)";
+  document.getElementById("mySidenav").style.width = "80%";
+  document.getElementById("close").style.position = "fixed";
+   document.getElementById("goto").style.position = "fixed";
+   document.getElementById("goto").style.right = "0%"
+   document.getElementById("goto").style.top = "-10px"
+   document.getElementById("goto").style.height = "100vh"
+   document.getElementById("goto").style.width = "20%"
+   document.getElementById("cover").style.position = "fixed" ;
+   document.getElementById("cover").style.left = "22%" ;
+   document.getElementById("settings").style.height = "0%";
+  document.getElementById("body").style.backgroundImage = "url(4.jpg)";
+  document.getElementById("body").style.backgroundImage = "url(./Theme/5.jpg)";
+}
+function e6() {
+  var audio = new Audio("Themechanged.wav");
+
+audio.play();
+  alert('bg applied');
+  document.getElementsByClassName("sidenav")[0].style.backgroundImage = "linear-gradient(to right, rgba(0,133,223,0.404) , rgba(255,22,22,0.326)";
+  document.getElementById("mySidenav").style.width = "80%";
+  document.getElementById("close").style.position = "fixed";
+   document.getElementById("goto").style.position = "fixed";
+   document.getElementById("goto").style.right = "0%"
+   document.getElementById("goto").style.top = "-10px"
+   document.getElementById("goto").style.height = "100vh"
+   document.getElementById("goto").style.width = "20%"
+   document.getElementById("cover").style.position = "fixed" ;
+   document.getElementById("cover").style.left = "22%" ;
+   document.getElementById("settings").style.height = "0%";
+  document.getElementById("body").style.backgroundImage = "url(6.jpg)";
+  document.getElementById("body").style.backgroundImage = "url(./Theme/6.jpg)";
+}
+function e7() {
+  var audio = new Audio("Themechanged.wav");
+
+audio.play();
+  alert('bg applied');
+  document.getElementsByClassName("sidenav")[0].style.backgroundImage = "linear-gradient(to right, rgba(194,0,136,0.204) , rgba(255,255,255,0.255)";
+  document.getElementById("mySidenav").style.width = "80%";
+  document.getElementById("close").style.position = "fixed";
+   document.getElementById("goto").style.position = "fixed";
+   document.getElementById("goto").style.right = "0%"
+   document.getElementById("goto").style.top = "-10px"
+   document.getElementById("goto").style.height = "100vh"
+   document.getElementById("goto").style.width = "20%"
+   document.getElementById("cover").style.position = "fixed" ;
+   document.getElementById("cover").style.left = "22%" ;
+   document.getElementById("settings").style.height = "0%";
+  document.getElementById("body").style.backgroundImage = "url(7.jpg)";
+  document.getElementById("body").style.backgroundImage = "url(./Theme/7.jpg)";
+}
+function e8() {
+  var audio = new Audio("Themechanged.wav");
+
+audio.play();
+  alert('bg applied');
+  document.getElementsByClassName("sidenav")[0].style.backgroundImage = "linear-gradient(to right, rgba(0,0,0,0.204) , rgba(255,121,121,0.309) , rgba(255,255,255,0.253)";
+  document.getElementById("mySidenav").style.width = "80%";
+  document.getElementById("close").style.position = "fixed";
+   document.getElementById("goto").style.position = "fixed";
+   document.getElementById("goto").style.right = "0%"
+   document.getElementById("goto").style.top = "-10px"
+   document.getElementById("goto").style.height = "100vh"
+   document.getElementById("goto").style.width = "20%"
+   document.getElementById("cover").style.position = "fixed" ;
+   document.getElementById("cover").style.left = "22%" ;
+   document.getElementById("settings").style.height = "0%";
+  document.getElementById("body").style.backgroundImage = "url(8.jpg)";
+  document.getElementById("body").style.backgroundImage = "url(./Theme/8.jpg)";
+}
+function e9() {
+  var audio = new Audio("Themechanged.wav");
+
+audio.play();
+  alert('bg applied');
+  document.getElementsByClassName("sidenav")[0].style.backgroundImage = "linear-gradient(to right, rgba(22,136,248,0.204) , rgba(255,255,255,0.255) , #effff ";
+  document.getElementById("mySidenav").style.width = "80%";
+  document.getElementById("close").style.position = "fixed";
+   document.getElementById("goto").style.position = "fixed";
+   document.getElementById("goto").style.right = "0%"
+   document.getElementById("goto").style.top = "-10px"
+   document.getElementById("goto").style.height = "100vh"
+   document.getElementById("goto").style.width = "20%"
+   document.getElementById("cover").style.position = "fixed" ;
+   document.getElementById("cover").style.left = "22%" ;
+   document.getElementById("settings").style.height = "0%";
+   document.getElementById("body").style.backgroundImage = "url(9.jpg)";
+   document.getElementById("body").style.backgroundImage = "url(./Theme/9.jpg)";
+}
+function e10() {
+  alert('bg applied');
+   document.getElementsByClassName("sidenav")[0].style.backgroundImage = "linear-gradient(to right, rgba(0,0,0,0.204) ,  rgba(255,121,121,0.309) , rgba(255,255,255,0.253)";
+   document.getElementById("mySidenav").style.width = "80%";
+   document.getElementById("close").style.position = "fixed";
+   document.getElementById("goto").style.position = "fixed";
+   document.getElementById("goto").style.right = "0%"
+   document.getElementById("goto").style.top = "-10px"
+   document.getElementById("goto").style.height = "100vh"
+   document.getElementById("goto").style.width = "20%"
+   document.getElementById("cover").style.position = "fixed" ;
+   document.getElementById("cover").style.left = "22%" ;
+   document.getElementById("settings").style.height = "0%";
+   document.getElementById("body").style.backgroundImage = "url(10.jpg)";
+   document.getElementById("body").style.backgroundImage = "url(./Theme/10.jpg)";
+}
+function a1() {
+  alert('bg applied');
+   document.getElementsByClassName("sidenav")[0].style.backgroundImage = "linear-gradient(to right, rgba(255,31,238,0.098) ,  rgba(255,2,2,0.15) , rgba(38,224,255,0.152)";
+   document.getElementById("mySidenav").style.width = "80%";
+   document.getElementById("close").style.position = "fixed";
+   document.getElementById("goto").style.position = "fixed";
+   document.getElementById("goto").style.right = "0%"
+   document.getElementById("goto").style.top = "-10px"
+   document.getElementById("goto").style.height = "100vh"
+   document.getElementById("goto").style.width = "20%"
+   document.getElementById("cover").style.position = "fixed" ;
+   document.getElementById("cover").style.left = "22%" ;
+   document.getElementById("settings").style.height = "0%";
+   document.getElementById("body").style.backgroundImage = "url(10.jpg)";
+   document.getElementById("body").style.backgroundImage = "url(./Theme/default/bg.jpg)";
+}
+function ad1() {
+  alert('bg applied');
+   document.getElementsByClassName("sidenav")[0].style.backgroundImage = "linear-gradient(to right, rgba(255,31,238,0.098) ,  rgba(255,2,2,0.15) , rgba(38,224,255,0.152)";
+   document.getElementById("mySidenav").style.width = "80%";
+   document.getElementById("close").style.position = "fixed";
+   document.getElementById("goto").style.position = "fixed";
+   document.getElementById("goto").style.right = "0%"
+   document.getElementById("goto").style.top = "-10px"
+   document.getElementById("goto").style.height = "100vh"
+   document.getElementById("goto").style.width = "20%"
+   document.getElementById("cover").style.position = "fixed" ;
+   document.getElementById("cover").style.left = "22%" ;
+   document.getElementById("settings").style.height = "0%";
+   document.getElementById("body").style.backgroundImage = "url(10.jpg)";
+   document.getElementById("body").style.backgroundImage = "url(./Theme/default/dbg1.jpg)";
+}
+function add() {
+  alert('bg applied');
+   document.getElementsByClassName("sidenav")[0].style.backgroundImage = "linear-gradient(to right, rgba(175,15,255,0.35) ,  rgba(160,2,255,0.15) , rgba(49,0,88,0.401)";
+   document.getElementById("mySidenav").style.width = "80%";
+   document.getElementById("close").style.position = "fixed";
+   document.getElementById("goto").style.position = "fixed";
+   document.getElementById("goto").style.right = "0%"
+   document.getElementById("goto").style.top = "-10px"
+   document.getElementById("goto").style.height = "100vh"
+   document.getElementById("goto").style.width = "20%"
+   document.getElementById("cover").style.position = "fixed" ;
+   document.getElementById("cover").style.left = "22%" ;
+   document.getElementById("settings").style.height = "0%";
+   document.getElementById("body").style.backgroundImage = "url(10.jpg)";
+   document.getElementById("body").style.backgroundImage = "url(./Theme/default/adbg.jpg)";
+}
